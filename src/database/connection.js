a11cd6cb786f134b569766f2856747412469d496
unknown <guilherme4garcia@gmail.com>
2022-09-17 16:02:53 -0300

    db update

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,12 +1,18 @@
-const mysql = require('mysql')
+const { Sequelize } = require('sequelize')
+
+// Option 1: Passing a connection URI
+const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
+
+/* const mysql = require('mysql')
 require('dotenv/config')
 
-const conexao = mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_CONNECTION,
-  database: 'database_name'
+  database: 'wmanager'
 })
 
-module.exports = conexao
+module.exports = connection
+ */
