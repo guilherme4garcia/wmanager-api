6e0d220c7a930a330d66f9ea571b71d17e7f8b56
unknown <guilherme4garcia@gmail.com>
2022-10-16 18:36:02 -0300

    admin crud

--- src/controller/AdminController.js
+++ src/controller/AdminController.js
@@ -24,6 +24,67 @@ class AdminController {
       console.log(error)
     }
   }
+
+  async listUser(req, res) {
+    const id = req.params.id
+    try {
+      const user = await User.findOne({ where: { uuid: id } })
+
+      if (user) {
+        const { uuid, name, email, admin } = user
+        const newUser = { uuid, name, email, admin }
+        res.status(200).send(newUser)
+      } else {
+        res.sendStatus(404)
+      }
+    } catch (error) {
+      res.sendStatus(400, error)
+    }
+  }
+
+  async list(req, res) {
+    const users = await User.findAll()
+    res.status(200).send(users)
+  }
+
+  async deleteUser(req, res) {
+    const id = req.params.id
+
+    try {
+      const user = await User.destroy({
+        where: {
+          uuid: id
+        },
+        force: true
+      })
+      res.sendStatus(200, user)
+    } catch (error) {
+      res.sendStatus(500, error)
+    }
+  }
+
+  async updateUser(req, res) {
+    const id = req.params.id
+    const name = req.body.name
+    const email = req.body.email
+    const admin = req.body.admin
+
+    try {
+      const user = await User.update(
+        { name: name, email: email, admin: admin },
+        {
+          where: {
+            uuid: id
+          }
+        }
+      )
+
+      console.log(user)
+      res.status(200).send(user)
+    } catch (error) {
+      res.sendStatus(500, error)
+    }
+  }
 }
 
 module.exports = AdminController
--- src/routes/Admin.routes.js
+++ src/routes/Admin.routes.js
@@ -6,4 +6,16 @@ const { signup, list, listUser, deleteUser, updateUser } = new AdminController()
 // CREATE A USER
 router.post('/signup', signup)
 
+// GETS A SINGLE USER FROM THE DATABASE
+router.get('/user/:id', listUser)
+
+// RETURNS ALL THE USERS IN THE DATABASE
+router.get('/users', list)
+
+//UPDATES A SINGLE USER IN THE DATABASE
+router.put('/id/:id', updateUser)
+
+// DELETES A USER FROM THE DATABASE
+router.delete('/id/:id', deleteUser)
+
 module.exports = router
