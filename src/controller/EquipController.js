ae3dab7e6438d1af1351774fc7fb4bf782662793
unknown <guilherme4garcia@gmail.com>
2022-11-13 19:07:55 -0300

    new routes

--- src/controller/EquipController.js
+++ src/controller/EquipController.js
@@ -1,15 +1,15 @@
 const Equip = require('../models/Equip')
 
 class EquipController {
-  async equip2user(req, res) {
+  async update_equip(req, res) {
     let id = req.body.id
 
     try {
       const equip = await Equip.update(
         {
-          user_id: req.body.user_id,
           ambiente: req.body.ambiente,
-          dt_saida: req.body.dt_saida
+          dt_entrada: req.body.emprestimo,
+          dt_saida: req.body.devolucao
         },
         {
           where: {
@@ -43,6 +43,87 @@ class EquipController {
   async list(req, res) {
     const equips = await Equip.findAll()
     res.status(200).send(equips)
+  }
+
+  async userEquipList(req, res) {
+    const equips = await Equip.findAll({
+      where: {
+        user_id: req.params.id
+      }
+    })
+
+    res.status(200).send(equips)
+  }
+
+  async removeEquip(req, res) {
+    const id = req.params.id
+    try {
+      const equip = await Equip.update(
+        {
+          user_id: null
+        },
+        {
+          where: {
+            uuid: id
+          }
+        }
+      )
+      console.log(equip)
+      res.status(200).send(equip)
+      
+    } catch (error) {
+      console.log(error)
+      res.sendStatus(500, error)
+    }
+  }
+
+  async changeUser(req, res) {
+    const id = req.body.id
+    const newUser = req.body.user
+    try {
+      const equip = await Equip.update(
+        {
+          user_id: newUser
+        },
+        {
+          where: {
+            uuid: id
+          }
+        }
+      )
+      console.log(equip)
+      res.status(200).send(equip)
+      
+    } catch (error) {
+      console.log(error)
+      res.sendStatus(500, error)
+    }
+  }
+
+  async update_equip_all(req, res) {
+    let id = req.body.id
+
+    try {
+      const equip = await Equip.update(
+        {
+          user_id: req.body.user,
+          ambiente: req.body.ambiente,
+          dt_entrada: req.body.emprestimo,
+          dt_saida: req.body.devolucao
+        },
+        {
+          where: {
+            uuid: id
+          }
+        }
+      )
+
+      console.log(equip)
+      res.status(200).send(equip)
+    } catch (error) {
+      console.log(error)
+      res.sendStatus(500, error)
+    }
   }
 }
 
--- src/routes/Equip.routes.js
+++ src/routes/Equip.routes.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const EquipController = require('../controller/EquipController')
-const { equip2user, newEquip, list } = new EquipController()
+const { update_equip_all, changeUser, removeEquip, userEquipList, update_equip, newEquip, list } = new EquipController()
 
 //CREATE NEW EQUIP
 router.post('/new-equip', newEquip)
 
 //SIGN EQUIP TO USER
-router.post('/equip2user', equip2user)
+router.put('/update-equip', update_equip)
 
 //LIST ALL EQUIPMENTS
 router.get('/equips', list)
+
+//LIST USER EQUIPMENTS
+router.get('/user/equips/:id', userEquipList)
+
+//REMOVER USER EQUIPMENT
+router.put('/user/equip/remove/:id', removeEquip)
+
+//CHANGE USER
+router.put('/change-user', changeUser)
+
+//Update Equip All Params
+router.put('/update-user', update_equip_all)
 
 module.exports = router
